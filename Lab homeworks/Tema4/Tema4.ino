2f419735eb700c125fd86e44d011cab38d18046f
Vlad-Stefan Stoleru <45101413+Darkonnas@users.noreply.github.com>
2019-11-13 07:13:39 +0200

    Add files via upload

---
+++ Lab homeworks/Tema4/Tema4.ino
@@ -0,0 +1,59 @@
+const int knockPin = A0;
+const int buzzerPin = 8;
+const int buttonPin = 2;
+
+void setup() {
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+  Serial.begin(9600);
+}
+
+bool buzzerActive = false;
+long int lastKnock = 0;
+
+int knockTreshold = 100;
+bool knockHappened = false;
+const long int knockWait = 5000;
+
+const int buzzerInterval = 100;
+long int lastBuzzerEvent = 0;
+bool buzzerState = false;
+
+void loop() {
+  if(buzzerActive) {
+    int buttonState = !digitalRead(buttonPin);
+    if(buttonState == true) {
+      Serial.println("Alarm stopped via button!");
+      digitalWrite(buzzerPin, LOW);
+      buzzerActive = false;
+      knockHappened = false;
+      buzzerState = false;
+    }
+    else {
+      if(millis() - lastBuzzerEvent >= buzzerInterval) {
+        buzzerState = !buzzerState;
+        digitalWrite(buzzerPin, buzzerState);
+        lastBuzzerEvent = millis();
+      }
+    }
+  }
+  else {
+    if(knockHappened) {
+       if(millis() - lastKnock >= knockWait) {
+         Serial.println("Alarm started at: " + String(millis()));
+         digitalWrite(buzzerPin, HIGH);
+         lastBuzzerEvent = millis();
+         buzzerActive = true;
+         buzzerState = true;
+       }
+    }
+    else {
+      int knockLevel = Serial.parseInt();
+      if(knockLevel >= knockTreshold) {
+        knockHappened = true;
+        lastKnock = millis();
+        Serial.println("Knock received at " + String(lastKnock));
+      }
+    }
+  }
+}
